c7ae9c8f2d8d0d1f11ab38cac672ad55494a5b2a
Ashish Amarnath <ashish.amarnath@gmail.com>
2019-09-25 17:17:39 -0700

    add infrastructure reference to core cluster yaml
    Signed-off-by: Ashish Amarnath <ashish.amarnath@gmail.com>

--- cmd/capi/capi.go
+++ cmd/capi/capi.go
@@ -43,8 +43,8 @@ func GetCoreCluster(name, namespace string, infraCluster object) *clusterv1.Clus
 	coreCluster.Name = name
 	coreCluster.Namespace = namespace
 	coreCluster.APIVersion = clusterv1.GroupVersion.String()
-	coreCluster.Spec.InfrastructureRef = referenceToObjectRef(infraCluster)
 	coreCluster.Spec = clusterv1.ClusterSpec{
+		InfrastructureRef: referenceToObjectRef(infraCluster),
 		ClusterNetwork: &clusterv1.ClusterNetwork{
 			Pods: &clusterv1.NetworkRanges{
 				CIDRBlocks: []string{"192.168.0.0/16"},
