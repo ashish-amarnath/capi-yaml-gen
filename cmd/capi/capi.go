f5b8069559e87029cdc5dd1751e29399274cedae
Chuck Ha <chuckh@vmware.com>
2019-11-01 10:44:28 -0400

    Update to v1alpha3
    Signed-off-by: Chuck Ha <chuckh@vmware.com>

--- cmd/capi/capi.go
+++ cmd/capi/capi.go
@@ -19,7 +19,7 @@ package capi
 import (
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	clusterv1 "sigs.k8s.io/cluster-api/api/v1alpha2"
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1alpha3"
 )
 
 type object interface {
@@ -64,12 +64,13 @@ func GetCoreMachine(name, namespace, clusterName, version string, bootstrapConfi
 	coreMachine.Name = name
 	coreMachine.Namespace = namespace
 	labels := map[string]string{
-		clusterv1.MachineClusterLabelName: clusterName,
+		clusterv1.ClusterLabelName: clusterName,
 	}
 	coreMachine.SetLabels(labels)
 	coreMachine.Spec.Bootstrap.ConfigRef = referenceToObjectRef(bootstrapConfig)
 	coreMachine.Spec.InfrastructureRef = *referenceToObjectRef(infraMachine)
 	coreMachine.Spec.Version = &version
+	coreMachine.Spec.ClusterName = clusterName
 	return coreMachine
 }
 
@@ -87,13 +88,15 @@ func GetCoreMachineDeployment(clusterName, name, namespace, version string, repl
 	dep.Name = name
 	dep.Namespace = namespace
 	dep.Spec.Replicas = &replicas
+	dep.Spec.ClusterName = clusterName
 
 	labels := map[string]string{
-		clusterv1.MachineClusterLabelName: clusterName,
+		clusterv1.ClusterLabelName: clusterName,
 	}
 	dep.Spec.Selector.MatchLabels = labels
 	dep.Spec.Template.ObjectMeta.Labels = labels
 	dep.Spec.Template.Spec.Version = &version
+	dep.Spec.Template.Spec.ClusterName = clusterName
 
 	// Set the machine template
 	dep.Spec.Template.Spec.InfrastructureRef.Name = machineTemplate.GetName()
